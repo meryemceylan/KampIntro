e3ba4cfaf2be95865d001017e171a6452a3fe85e
Meryem Ceylan <xmeryemceylan@gmail.com>
2021-01-19 15:55:19 +0300

    19 Ocak

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -6,13 +6,19 @@ namespace ClassMetotDemo
 {
     class MusteriManager
     {
-       public void Listele(Musteri musteri)
+        public void Ekle(Musteri musteri)
         {
-            Console.WriteLine("Müşteriler listelendi");
+            Console.WriteLine("Müşteri eklendi : " + musteri.Name + " " + musteri.Surname);
+        }
+        public void Listele()
+        {
+            Console.WriteLine("Müşteriler listelendi : ");
+            
+            //Console.WriteLine( musteri.ID + " " + musteri.Name + " " + musteri.Surname + " " + musteri.Tc + " " + musteri.Number + " " + musteri.City);
         }
         public void Sil(Musteri musteri)
         {
-            Console.WriteLine("Üyelik sonlandırıldı");
+            Console.WriteLine("Üyelik sonlandırıldı : " + musteri.Name + " " + musteri.Surname);
         }
     }
 }
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,46 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 1;
+            musteri1.Name = "Mirza";
+            musteri1.Surname = "Aslan";
+            musteri1.Tc = "11111111111";
+            musteri1.Number = "00000000001";
+            musteri1.City = "İstanbul";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 2;
+            musteri2.Name = "Hazal";
+            musteri2.Surname = "Kaya";
+            musteri2.Tc = "11111111112";
+            musteri2.Number = "00000000002";
+            musteri2.City = "Kayseri";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.ID = 3;
+            musteri3.Name = "Meryem";
+            musteri3.Surname = "Ceylan";
+            musteri3.Tc = "11111111113";
+            musteri3.Number = "00000000003";
+            musteri3.City = "Sivas";
+
+            Console.WriteLine("-----EKLE-----");
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            Console.WriteLine("-----LİSTELE-----");
+            musteriManager.Listele();
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.ID + " " + musteri.Name + " " + musteri.Surname + " " + 
+                    musteri.Tc + " " + musteri.Number + " " + musteri.City);
+            }
+
+            Console.WriteLine("-----SİL-----");
+            musteriManager.Sil(musteri3);
         }
     }
 }
